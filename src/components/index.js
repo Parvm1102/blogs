a3c1336e280717623b7b9448c911e71667f8ee69
Parvm1102 <parvmittal31757@gmail.com>
2025-05-30 23:42:42 +0530

    feat: add authentication components including Login, Signup, and Protected routes

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,10 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
-
-export {Header, Footer, Container, LogoutBtn, Logo}
+import Button from "./Button";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Select from "./Select";
+import Signup from "./Signup";
+export {Header, Footer, Container, LogoutBtn, Logo, Button, Input, PostCard, Login, Select, Signup}
